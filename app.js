e8b2a958aedce056d2304716b851b746abdfe0f1
mrKarlJensen <mrkarljensen@gmail.com>
2017-05-16 16:19:31 -0400

    Added js for random quote. Search bar CSS
    Added an array of quotes and js to pull a random quote to display in the
    new tab. CSS on the search google box for transparent unless focused.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+/**
 var app = angular.module('sawyerSays', []);
 
 app.controller('MainCtrl', [
@@ -9,5 +11,16 @@ app.controller('MainCtrl', [
 
 
 	}]);
-			
+	**/		
 
+var sawyerSays = [
+
+"Rileys are cute.", 
+"This is how you stand when you don't have to prove anything to anyone.", 
+"I have rainbow powers, and when I run rainbows blast out of my shoes."
+
+];
+
+var enterQuote = Math.floor( Math.random() * sawyerSays.length );
+
+document.write ( sawyerSays[ enterQuote ] );
